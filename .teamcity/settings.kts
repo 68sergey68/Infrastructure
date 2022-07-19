cffeee1304777bbeeb4376f65af973772b9ee357
admin <admin@TeamCity>
2022-07-19 12:36:00 +0000

    TeamCity change in 'Infrastructure / Deckhouse' project: build step was copied in 'ClusterInstall' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -104,6 +104,13 @@ object Infrastructure_Deckhouse_ClusterInstall : BuildType({
             dockerImage = "registry.deckhouse.io/deckhouse/ce/install:stable"
             dockerRunParameters = """-v "${'$'}PWD/config.yml:/config.yml" -v "${'$'}HOME/.ssh/:/tmp/.ssh/" -v "${'$'}PWD/resources.yml:/resources.yml" -v "${'$'}PWD/dhctl-tmp:/tmp/dhctl""""
         }
+        exec {
+            name = "Run Deckhouse with settings (1)"
+            path = "dhctl bootstrap --ssh-user=ubuntu --ssh-agent-private-keys=/tmp/.ssh/id_rsa --config=/config.yml --resources=/resources.yml"
+            dockerPull = true
+            dockerImage = "registry.deckhouse.io/deckhouse/ce/install:stable"
+            dockerRunParameters = """-v "${'$'}PWD/config.yml:/config.yml" -v "${'$'}HOME/.ssh/:/tmp/.ssh/" -v "${'$'}PWD/resources.yml:/resources.yml" -v "${'$'}PWD/dhctl-tmp:/tmp/dhctl""""
+        }
         script {
             name = "Remove setting after deploy"
             scriptContent = "rm -rf resources.yml config.yml"
